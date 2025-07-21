e927cad111be80dcf1b1c07123c818310f12a4d5
Mtanvir <tanviranjum201@gmail.com>
2025-07-22 02:34:31 +0600

    Completed the 10th lecture

---
+++ L9_Associativity.java
@@ -0,0 +1,8 @@
+public class L9_Associativity {
+    public static void main(String[] args) {
+        int a= 64/4-3*4;
+        System.out.println(a);
+        int b = 5*3-64/4;
+        System.out.println(b);
+    }
+}
